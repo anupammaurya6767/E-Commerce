93459f956e107cead3f36edb3fda18da8d68a36b
Anupam Maurya <anupammaurya6767@gmail.com>
2024-07-10 16:58:53 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,16 +1,21 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = `mongodb+srv://noobkoda:noobkoda2004@expresso.7tk1f8j.mongodb.net/?retryWrites=true&w=majority`
-    try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
-        console.log('Database Connected Succesfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
-    }
+    const DB_NAME = "ECOMMERCE"; // Replace with your database name
 
+    // MongoDB Atlas URI with `mongodb+srv://`
+    const URI = `mongodb+srv://noobkoda:noobkoda2004@expresso.7tk1f8j.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('Database Connected Successfully');
+    } catch (error) {
+        console.error('Error connecting to database:', error.message);
+    }
 };
 
 export default Connection;
